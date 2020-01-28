4bbdc08aff1bf87eb5689844397b77b5cc78105d
Danny Clyde <Dantheman27c@gmail.com>
2020-01-28 00:04:24 -0700

    refactoring models and creating superuser

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Order, Customer, Part, Item
 
 # Register your models here.
+admin.register(Order)
+admin.register(Customer)
+admin.register(Part)
+admin.register(Item)
--- orders/models.py
+++ orders/models.py
@@ -1,40 +1,44 @@
 from django.db import models
 
+
 # Create your models here.
-class State(models.Model):
-  code = models.CharField(primary_key=True, max_length=2)
-  saleTax = models.FloatField()
-  def __str__(self):
-    return self.code
 
 class Customer(models.Model):
-  _id = models.CharField(primary_key=True, max_length=20)
-  name = models.CharField(max_length=20)
-  def __str__(self):
-    return self._id + ': ' + self.name
+    _id = models.CharField(primary_key=True, max_length=20)
+    name = models.CharField(max_length=20)
+    email = models.EmailField(null=True)
+
+    def __str__(self):
+        return str(self._id) + ': ' + str(self.name)
+
 
 class Part(models.Model):
-  _id = models.CharField(primary_key=True, max_length=20)
-  unitCost = models.IntegerField(default=0)
-  name = models.CharField(max_length=50)
-  imgName = models.CharField(max_length=50, default='')
-  obsolete = models.BooleanField(default=False)
-  def __str__(self):
-    return self._id + ': ' + self.name
+    _id = models.CharField(primary_key=True, max_length=20)
+    unitCost = models.IntegerField(default=0)
+    name = models.CharField(max_length=50)
+    obsolete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self._id) + str(self.name)
+
 
 class Order(models.Model):
-  _id = models.CharField(primary_key=True, max_length=20)
-  customerNumber = models.ForeignKey(Customer, on_delete=models.PROTECT)
-  shippingTotal = models.IntegerField(default=0)
+    _id = models.CharField(primary_key=True, max_length=20)
+    customerNumber = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    shippingTotal = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self._id) + str(self.customerNumber)
+
 
 class Item(models.Model):
-  _id = models.AutoField(primary_key=True)
-  sequenceNumber = models.IntegerField()
-  orderNumber = models.ForeignKey(Order, on_delete=models.CASCADE)
-  partNumber = models.ForeignKey(Part, on_delete=models.PROTECT)
-  quantity = models.IntegerField()
+    _id = models.AutoField(primary_key=True)
+    sequenceNumber = models.IntegerField()
+    orderNumber = models.ForeignKey(Order, on_delete=models.CASCADE)
+    partNumber = models.ForeignKey(Part, on_delete=models.PROTECT)
+    quantity = models.IntegerField()
 
-class CustomerState(models.Model):
-  _id = models.AutoField(primary_key=True)
-  customerNumber = models.ForeignKey(Customer, on_delete=models.PROTECT)
-  state = models.ForeignKey(State, to_field='code', on_delete=models.PROTECT)
+    def __str__(self):
+        return str(self._id) + str(self.quantity) + ' x ' + str(self.partNumber)
+
+
